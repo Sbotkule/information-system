72228c5a689b3b088d1d2f56b6ac387131a8cb3e
Sbotkule <152930116+Sbotkule@users.noreply.github.com>
2023-12-06 21:27:22 +0000

    Update greeting.py

--- greeting.py
+++ greeting.py
@@ -13,7 +13,15 @@ def helloall():
     return "Hello {}!".format(name)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, ssl_context=('/etc/letsencrypt/live/sakshi.uksouth.cloudapp.azure.com/fullchain.pem', '/etc/letsencrypt/live/sakshi.uksouth.cloudapp.azure.com/privkey.pem'))
-print("Script executed.")
+    app.run(
+        host='0.0.0.0',
+        port=8080,
+        ssl_context=(
+            '/etc/letsencrypt/live/sakshi.uksouth.cloudapp.azure.com/fullchain.pem',
+            '/etc/letsencrypt/live/sakshi.uksouth.cloudapp.azure.com/privkey.pem'
+        )
+    )
+    print("Script executed.")
+
 
 
