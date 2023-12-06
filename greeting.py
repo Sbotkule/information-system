96352138ec196512bad376f125edc04144e222a9
Sbotkule <152930116+Sbotkule@users.noreply.github.com>
2023-12-06 21:22:45 +0000

    Update greeting.py

--- greeting.py
+++ greeting.py
@@ -14,4 +14,6 @@ def helloall():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, ssl_context=('/etc/letsencrypt/live/sakshi.uksouth.cloudapp.azure.com/fullchain.pem', '/etc/letsencrypt/live/sakshi.uksouth.cloudapp.azure.com/privkey.pem'))
+print("Script executed.")
+
 
